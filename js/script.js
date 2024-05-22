9f53ba932e09b5ac3e4b9d3e8fae3b6b2e2993e5
Mahedere <mahederetebebe98@gmail.com>
2024-05-22 13:02:45 -0700

    scroll testimonials function completed

--- js/script.js
+++ js/script.js
@@ -6,3 +6,29 @@ btn.addEventListener('click',()=>{
     nav.classList.toggle('flex')
     nav.classList.toggle('hidden')
 })
+document.addEventListener('DOMContentLoaded', (event) => {
+    const container = document.getElementById('testimonials');
+    const progressBar = document.getElementById('progress-bar');
+    const circles = progressBar.querySelectorAll('div');
+
+    function updateProgress() {
+        const containerWidth = container.scrollWidth - container.clientWidth;
+        const scrollLeft = container.scrollLeft;
+        const scrollFraction = scrollLeft / containerWidth;
+
+        const activeIndex = Math.round(scrollFraction * (circles.length - 1));
+
+        circles.forEach((circle, index) => {
+            if (index === activeIndex) {
+                circle.classList.add('bg-BrightRed');
+                circle.classList.remove('bg-gray-300');
+            } else {
+                circle.classList.add('bg-gray-300');
+                circle.classList.remove('bg-BrightRed');
+            }
+        });
+    }
+
+    container.addEventListener('scroll', updateProgress);
+    updateProgress();
+});
